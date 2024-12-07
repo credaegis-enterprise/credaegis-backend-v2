fe2a0e53f1696e1022f9f573d5844d854f70e1d5
AllenBastian <reachallenbastian@gmail.com>
2024-12-07 19:19:16 +0530

    fix: role not saving

--- src/main/java/com/credaegis/backend/service/MemberService.java
+++ src/main/java/com/credaegis/backend/service/MemberService.java
@@ -2,12 +2,14 @@ package com.credaegis.backend.service;
 
 
 import com.credaegis.backend.Constants;
+import com.credaegis.backend.entity.Role;
 import com.credaegis.backend.http.request.MemberCreationRequest;
 import com.credaegis.backend.entity.Cluster;
 import com.credaegis.backend.entity.User;
 import com.credaegis.backend.exception.custom.ExceptionFactory;
 import com.credaegis.backend.repository.ClusterRepository;
 import com.credaegis.backend.repository.OrganizationRepository;
+import com.credaegis.backend.repository.RoleRepository;
 import com.credaegis.backend.repository.UserRepository;
 import com.github.f4b6a3.ulid.UlidCreator;
 import jakarta.transaction.Transactional;
@@ -26,6 +28,7 @@ public class MemberService {
     private final UserRepository userRepository;
     private final ClusterRepository clusterRepository;
     private final OrganizationRepository organizationRepository;
+    private final RoleRepository roleRepository;
 
 
     public void createMember(MemberCreationRequest memberCreationRequest, String userOrganizationId) {
@@ -37,14 +40,19 @@ public class MemberService {
         if (cluster.getOrganization().getId().equals(userOrganizationId)) {
             if (member == null || member.isDeleted()) {
                 User user = new User();
+                Role role = new Role();
                 user.setId(UlidCreator.getUlid().toString());
                 user.setPassword("sgce");
                 user.setUsername(memberCreationRequest.getUsername());
                 user.setEmail(memberCreationRequest.getEmail());
                 user.setCluster(cluster);
+                role.setId(UlidCreator.getUlid().toString());
+                role.setUser(user);
+                role.setRole("ROLE_"+Constants.MEMBER);
                 user.setOrganization(organizationRepository.findById(userOrganizationId).orElseThrow(ExceptionFactory::resourceNotFound));
 
                 userRepository.save(user);
+                roleRepository.save(role);
             } else throw ExceptionFactory.customValidationError("Member already exists, try another email");
 
         } else throw ExceptionFactory.insufficientPermission();
