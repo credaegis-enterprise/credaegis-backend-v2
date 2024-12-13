b1f9535d9ae90f3508f236276fad25d720c399fb
AllenBastian <reachallenbastian@gmail.com>
2024-12-13 14:38:19 +0530

    status: working with pageable

--- src/main/java/com/credaegis/backend/repository/CertificateRepository.java
+++ src/main/java/com/credaegis/backend/repository/CertificateRepository.java
@@ -1,10 +1,15 @@
 package com.credaegis.backend.repository;
 
 import com.credaegis.backend.entity.Certificate;
+import com.credaegis.backend.entity.Organization;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface  CertificateRepository extends JpaRepository<Certificate,String> {
     Optional<Certificate> findByCertificateHash(String hashedValue);
+
+    Page<Certificate> findByEvent_Cluster_Organization(Organization organization, Pageable pageable);
 }
