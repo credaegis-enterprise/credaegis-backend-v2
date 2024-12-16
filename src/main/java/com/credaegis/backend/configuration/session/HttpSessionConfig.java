3dc61eb34052856b4c2b8a61e29cb79f73db5779
AllenBastian <reachallenbastian@gmail.com>
2024-12-16 18:48:28 +0530

    fix: working and added cookie serializer

--- src/main/java/com/credaegis/backend/configuration/session/HttpSessionConfig.java
+++ src/main/java/com/credaegis/backend/configuration/session/HttpSessionConfig.java
@@ -1,11 +1,22 @@
 package com.credaegis.backend.configuration.session;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
 @EnableRedisHttpSession
 public class HttpSessionConfig {
+
+
+    @Bean
+    CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("ORGANIZATION_SESSION");
+        return serializer;
+    }
 
 }
