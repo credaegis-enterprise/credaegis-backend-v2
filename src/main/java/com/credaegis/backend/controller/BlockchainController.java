e3961816abcd00c9afb3ce486a732f0e553ba0bb
AllenBastian <reachallenbastian@gmail.com>
2025-01-06 21:49:07 +0530

    added: get balanc controller

---
+++ src/main/java/com/credaegis/backend/controller/BlockchainController.java
@@ -0,0 +1,25 @@
+package com.credaegis.backend.controller;
+
+
+import com.credaegis.backend.constant.Constants;
+import com.credaegis.backend.http.response.api.CustomApiResponse;
+import com.credaegis.backend.service.BlockchainService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = Constants.ROUTEV1+"/web3")
+@AllArgsConstructor
+public class BlockchainController {
+
+    private final BlockchainService blockchainService;
+
+    @GetMapping("/get-balance")
+    public ResponseEntity<CustomApiResponse<Long>> getBalance(){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new CustomApiResponse<>(blockchainService.getBalance(),"Account balance",true)
+        );
+    }
+}
