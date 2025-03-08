59bc5bf63aaeb990db77b020392308155a2f76c7
AllenBastian <reachallenbastian@gmail.com>
2025-03-08 15:00:43 +0530

    feat: added web3 controllers

--- src/main/java/com/credaegis/backend/controller/organization/Web3Controller.java
+++ src/main/java/com/credaegis/backend/controller/organization/Web3Controller.java
@@ -2,26 +2,105 @@ package com.credaegis.backend.controller.organization;
 
 
 import com.credaegis.backend.constant.Constants;
+import com.credaegis.backend.dto.ContractStateDTO;
+import com.credaegis.backend.dto.HashBatchInfoDTO;
+import com.credaegis.backend.http.request.MerkleRootVerificationRequest;
 import com.credaegis.backend.http.response.api.CustomApiResponse;
+import com.credaegis.backend.http.response.custom.BlockchainInfoResponse;
+
 import com.credaegis.backend.service.organization.Web3Service;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping(value = Constants.ROUTE_V1_ORGANIZATION +"/web3")
-@AllArgsConstructor
+@RequestMapping(value = Constants.ROUTE_V1_ORGANIZATION+"/web3")
+@RequiredArgsConstructor
 public class Web3Controller {
 
     private final Web3Service web3Service;
 
-    @GetMapping("/get-balance")
+
+
+
+    @GetMapping("/public/txn/{hash}")
+    public ResponseEntity<CustomApiResponse<String>> getTransactionReceipt(@PathVariable String hash){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new CustomApiResponse<>(web3Service.getTxnDetails(hash),"Transaction receipt fetched",true)
+        );
+    }
+    @PostMapping("/public/store/current/merkle-root")
+    public ResponseEntity<CustomApiResponse<Void>> storeCurrentMerkleRootToPublic(){
+        web3Service.storeCurrentBatchMerkleRootToPublic();
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new CustomApiResponse<>(null,"Request is being processed",true)
+        );
+    }
+
+
+    @GetMapping("/public/verify/merkle-roots")
+    public ResponseEntity<CustomApiResponse<Map<String,Boolean>>> verifyMerkleRootPublic(@RequestBody MerkleRootVerificationRequest merkleRoots){
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new CustomApiResponse<>(       web3Service.verifyMerkleRootPublic(merkleRoots.getMerkleRoots()),"Merkle root verified",true)
+        );
+    }
+
+
+
+    @GetMapping("/private/current-batch/merkle-root")
+    public ResponseEntity<CustomApiResponse<String>> getMerkleRootCurrentBatch(){
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new CustomApiResponse<>(web3Service.getCurrentBatchMerkleRoot(),"Merkle root of current batch",true)
+        );
+    }
+
+
+    @GetMapping("/public/info")
+    public ResponseEntity<CustomApiResponse<BlockchainInfoResponse>> getWeb3Info(){
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new CustomApiResponse<>( web3Service.getBlockchainInfo(),"Blockchain info",true)
+        );
+    }
+
+    @GetMapping("/private/batch/{id}")
+    public ResponseEntity<CustomApiResponse<HashBatchInfoDTO>> getBatchInfo(@PathVariable  String id){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new CustomApiResponse<>(web3Service.getBatchInfo(id),"Batch info",true)
+        );
+    }
+
+    @GetMapping("/public/get-balance")
     public ResponseEntity<CustomApiResponse<String>> getBalance(){
         return ResponseEntity.status(HttpStatus.OK).body(
-                new CustomApiResponse<>(web3Service.getBalance(),"Account balance in Ethers",true)
+                new CustomApiResponse<>(web3Service.getBalance(),"Account balance in Ethers (Avalanche)",true)
+        );
+    }
+
+
+
+
+
+    @GetMapping("/private/current-batch")
+    public ResponseEntity<CustomApiResponse<HashBatchInfoDTO>> getCurrentBatchInfo(){
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new CustomApiResponse<>(web3Service.getCurrentBatchInfo(),"Current batch info",true)
+        );
+    }
+
+
+    @GetMapping("/private/contract-state")
+    public ResponseEntity<CustomApiResponse<ContractStateDTO>> getContractState(){
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new CustomApiResponse<>(     web3Service.getContractState(),"Contract state",true)
         );
     }
 
